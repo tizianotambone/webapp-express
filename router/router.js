6c1389733bb3d2ce20ca8311550998dd60b8a4c9
tizianotambone <tambonetiziano@gmail.com>
2025-05-29 19:33:22 +0200

    [UPD] ex done

--- router/router.js
+++ router/router.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const filmController = require('../controller/filmController');
+
+
+router.get('/', filmController.index)
+router.get('/:id', filmController.show)
+
+module.exports = router;
