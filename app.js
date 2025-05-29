5b241214a0bca1543d0259a6f273d2651aed7d77
tizianotambone <tambonetiziano@gmail.com>
2025-05-29 19:30:20 +0200

    [UPD]app.js

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
+
 app.use(express.static('public'));
 
+const filmRouter = require('./router/router');
+
+app.use('/api/movies', filmRouter);
 
 
+
+app.get('/', (req, res) => {
+    res.send("welcome to my movie list");
+})
 
 app.listen(port, () => {
     console.log(`Server is running at port:${port}`);
